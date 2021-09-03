7ddd4997d95b38f4755c6c99e8c1e755acc45844
adrianlukito <lukito.adrian1@gmail.com>
2021-09-03 16:45:28 +0700

    Initial Commit

---
+++ core/src/main/java/com/lionparcel/commonandroid/LibraryLog.kt
@@ -0,0 +1,22 @@
+package com.lionparcel.commonandroid
+
+import android.util.Log
+
+class LibraryLog {
+
+    companion object {
+        private const val TAG = "LibraryLog"
+
+        fun debug(message: String) {
+            Log.d(TAG, message)
+        }
+    }
+
+    internal fun asd() {
+        Log.d(TAG, "TEST")
+    }
+
+    fun dsa() {
+        Log.d(TAG, "DSA")
+    }
+}
