9155d5fef103e2c2aab787fc6b2e40587ad67e12
Falih Mulyana <falihakz@gmail.com>
2021-09-10 13:52:45 +0700

    init skeleton file and classes for form components

---
+++ sample/src/main/java/com/lionparcel/commonandroidsample/FormComponentSampleActivity.kt
@@ -0,0 +1,12 @@
+package com.lionparcel.commonandroidsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormComponentSampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        title = "Form Component Sample"
+        setContentView(R.layout.activity_form_component_sample)
+    }
+}
