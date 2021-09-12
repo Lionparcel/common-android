c1f70cbb869961360fb206e9c2121c1a68a5dd2d
Falih Mulyana <falihakz@gmail.com>
2021-09-13 00:59:57 +0700

    experimenting with theme and style for TextInputLayout

--- sample/src/main/java/com/lionparcel/commonandroidsample/FormComponentSampleActivity.kt
+++ sample/src/main/java/com/lionparcel/commonandroidsample/FormComponentSampleActivity.kt
@@ -2,11 +2,14 @@ package com.lionparcel.commonandroidsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.textfield.TextInputLayout
 
 class FormComponentSampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         title = "Form Component Sample"
         setContentView(R.layout.activity_form_component_sample)
+
+        findViewById<TextInputLayout>(R.id.tilEmail).error = "this is an error"
     }
 }
