5f876650b8c0b68e145f221b3dc8e65673be9711
Falih Mulyana <falihakz@gmail.com>
2021-09-13 11:00:12 +0700

    begin creating custom views

---
+++ core/src/main/java/com/lionparcel/commonandroid/form/LPTextInputEditText.kt
@@ -0,0 +1,11 @@
+package com.lionparcel.commonandroid.form
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.textfield.TextInputEditText
+
+class LPTextInputEditText @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+): TextInputEditText(context, attrs, defStyleAttr)
