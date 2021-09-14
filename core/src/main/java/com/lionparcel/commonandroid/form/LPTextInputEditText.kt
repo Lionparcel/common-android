164fa72c00d9320e1e97c36a0f893d91d6554681
Falih Mulyana <falihakz@gmail.com>
2021-09-14 22:22:18 +0700

    remove unused imports and variable

--- core/src/main/java/com/lionparcel/commonandroid/form/LPTextInputEditText.kt
+++ core/src/main/java/com/lionparcel/commonandroid/form/LPTextInputEditText.kt
@@ -1,18 +1,12 @@
 package com.lionparcel.commonandroid.form
 
 import android.content.Context
-import android.content.res.Resources
-import android.graphics.Canvas
 import android.util.AttributeSet
 import android.view.ContextThemeWrapper
 import com.google.android.material.textfield.TextInputEditText
 import com.lionparcel.commonandroid.R
 
 class LPTextInputEditText: TextInputEditText {
-
-    companion object {
-        private const val MINIMUM_HEIGHT_PX = 55
-    }
 
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet? = null) : this(context, attrs, 0)
