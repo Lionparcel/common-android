ffe509ac0d65dfafe886dd72087bd4c4d537c13f
Falih Mulyana <falihakz@gmail.com>
2021-09-14 20:58:30 +0700

    set default style attribute to the custom view constructor

--- core/src/main/java/com/lionparcel/commonandroid/form/LPTextInputEditText.kt
+++ core/src/main/java/com/lionparcel/commonandroid/form/LPTextInputEditText.kt
@@ -1,11 +1,21 @@
 package com.lionparcel.commonandroid.form
 
 import android.content.Context
+import android.content.res.Resources
+import android.graphics.Canvas
 import android.util.AttributeSet
+import android.view.ContextThemeWrapper
 import com.google.android.material.textfield.TextInputEditText
+import com.lionparcel.commonandroid.R
 
-class LPTextInputEditText @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-): TextInputEditText(context, attrs, defStyleAttr)
+class LPTextInputEditText: TextInputEditText {
+
+    companion object {
+        private const val MINIMUM_HEIGHT_PX = 55
+    }
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet? = null) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int)
+        : super(ContextThemeWrapper(context, R.style.LPTextInputEditText), attrs, defStyleAttr)
+}
--- core/src/main/java/com/lionparcel/commonandroid/form/LPTextInputLayout.kt
+++ core/src/main/java/com/lionparcel/commonandroid/form/LPTextInputLayout.kt
@@ -2,10 +2,14 @@ package com.lionparcel.commonandroid.form
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.ContextThemeWrapper
 import com.google.android.material.textfield.TextInputLayout
+import com.lionparcel.commonandroid.R
 
-class LPTextInputLayout @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-): TextInputLayout(context, attrs, defStyleAttr)
+class LPTextInputLayout: TextInputLayout {
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
+        : super(ContextThemeWrapper(context, R.style.LPTextInputLayout), attrs, defStyleAttr)
+}
