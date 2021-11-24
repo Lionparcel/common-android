83ab270f50eae79055166ab7472d003104b2a7e7
Falih Mulyana <falihakz@gmail.com>
2021-11-24 17:24:23 +0700

    reimplement ctyle

--- core/src/main/java/com/lionparcel/commonandroid/form/LPTextInputLayout.kt
+++ core/src/main/java/com/lionparcel/commonandroid/form/LPTextInputLayout.kt
@@ -15,7 +15,7 @@ class LPTextInputLayout: TextInputLayout {
     constructor(context: Context, attrs: AttributeSet? = null)
         : this(context, attrs, R.attr.LPTextInputLayoutStyle)
     constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int)
-        : super(context, attrs, defStyleAttr)
+        : super(ContextThemeWrapper(context, R.style.LPTextInputLayoutTheme), attrs, defStyleAttr)
 
     init {
         val prefixView = this.findViewById<AppCompatTextView>(com.google.android.material.R.id.textinput_prefix_text)
@@ -23,6 +23,6 @@ class LPTextInputLayout: TextInputLayout {
             ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT)
         prefixView.gravity = Gravity.CENTER
 
-        this.context.setTheme(R.style.LPTextInputLayoutTheme)
+        context.setTheme(R.style.LPTextInputLayoutTheme)
     }
 }
