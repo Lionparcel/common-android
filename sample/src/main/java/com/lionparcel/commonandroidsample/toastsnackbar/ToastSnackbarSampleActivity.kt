d0442d0232990d6de0c3c2874635bfb06ec4d746
risanjayy <risyadul@thelionparcel.com>
2021-11-26 03:17:08 +0700

    add sample page toast snackbar

---
+++ sample/src/main/java/com/lionparcel/commonandroidsample/toastsnackbar/ToastSnackbarSampleActivity.kt
@@ -0,0 +1,31 @@
+package com.lionparcel.commonandroidsample.toastsnackbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import com.lionparcel.commonandroid.snackbartoast.showToastDefaultBasicNoClose
+import com.lionparcel.commonandroid.snackbartoast.showToastDefaultButtonText
+import com.lionparcel.commonandroid.snackbartoast.showToastDefaultLargeIconNoClose
+import com.lionparcel.commonandroid.snackbartoast.showToastDefaultSmallIconNoClose
+import com.lionparcel.commonandroidsample.R
+
+class ToastSnackbarSampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toast_snackbar_sample)
+        val parent = findViewById<LinearLayout>(R.id.parent)
+        findViewById<Button>(R.id.btnToastDefaultBasicNoClose).setOnClickListener {
+            showToastDefaultBasicNoClose(parent, getString(R.string.general_message))
+        }
+        findViewById<Button>(R.id.btnToastDefaultSmallIconNoClose).setOnClickListener {
+            showToastDefaultSmallIconNoClose(parent, getString(R.string.general_message), R.drawable.ics_f_check_circle)
+        }
+        findViewById<Button>(R.id.btnToastDefaultLargeIconNoClose).setOnClickListener {
+            showToastDefaultLargeIconNoClose(parent, getString(R.string.general_message), R.drawable.ics_f_info_circle)
+        }
+        findViewById<Button>(R.id.btnToastDefaultButtonText).setOnClickListener {
+            showToastDefaultButtonText(parent, getString(R.string.general_message), "Label Text", callbackMessageButton = {})
+        }
+    }
+}
