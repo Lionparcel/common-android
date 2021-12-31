520ed039f10a4da8e1d7a295af1c8398072731c1
risanjayy <risyadul@thelionparcel.com>
2021-12-31 15:31:18 +0700

    improve attach file code

--- core/src/main/java/com/lionparcel/commonandroid/form/LPAttachFile.kt
+++ core/src/main/java/com/lionparcel/commonandroid/form/LPAttachFile.kt
@@ -8,7 +8,10 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import com.lionparcel.commonandroid.R
@@ -19,17 +22,33 @@ import java.io.File
 
 class LPAttachFile : FrameLayout {
 
-    var fileUri: Uri? = null
+    companion object {
+        private const val KTP_PHOTO_KEY = "KTP_PHOTO_FILE_URI_KEY"
+        private const val PACKAGE_PHOTO_REQUIRED_KEY = "PACKAGE_PHOTO_REQUIRED_FILE_URI_KEY"
+    }
 
+    var fileUri: Uri? = null
     var selectedImage: File? = null
+    var previewPhotoCallback: ((File) -> Unit)? = null
+    var gotoCameraCallback: ((Uri) -> Unit)? = null
+    var gotoGalleryCallback: ((Uri) -> Unit)? = null
+    var isOptional: Boolean = false
+    var isPhotoEnabled: Boolean = true
+    var onResetPhotoCallback: ((selectedImage: File?, fileUri: Uri?) -> Unit)? = null
 
     var activity: Activity? = null
 
-    var previewPhoto: ((File) -> Unit)? = null
-
-    var gotoCamera: ((Uri) -> Unit)? = null
-
-    var gotoGallery: ((Uri) -> Unit)? = null
+    var clParent: ConstraintLayout
+    var txtPhotoLabel: TextView
+    var llPhoto: LinearLayout
+    var txtPhotoInfo: TextView
+    var txtPhotoRequiredLabel: TextView
+    var llEmptyPhoto: TextView
+    var clPhotoDetail: ConstraintLayout
+    var imgPhoto: ImageView
+    var txtPhotoName: TextView
+    var txtPhotoSize: TextView
+    var txtPhotoAction: TextView
 
     constructor(context: Context) : this(context, null)
 
@@ -42,18 +61,29 @@ class LPAttachFile : FrameLayout {
     ) {
         val layoutInflater = LayoutInflater.from(context)
         layoutInflater.inflate(R.layout.attach_file_view, this, true)
-
-        with(includeKtpPhoto) {
-            llEmptyPhoto.setOnClickListener {
+        clParent = findViewById(R.id.clParent)
+        txtPhotoLabel = findViewById(R.id.txtPhotoLabel)
+        llPhoto = findViewById(R.id.llPhoto)
+        txtPhotoInfo = findViewById(R.id.txtPhotoInfo)
+        txtPhotoRequiredLabel = findViewById(R.id.txtPhotoRequiredLabel)
+        llEmptyPhoto = findViewById(R.id.llEmptyPhoto)
+        clPhotoDetail = findViewById(R.id.clPhotoDetail)
+        imgPhoto = findViewById(R.id.imgPhoto)
+        txtPhotoName = findViewById(R.id.txtPhotoName)
+        txtPhotoSize = findViewById(R.id.txtPhotoSize)
+        txtPhotoAction = findViewById(R.id.txtPhotoAction)
+        txtPhotoLabel.isVisible = !isOptional
+        llEmptyPhoto.setOnClickListener {
+            addPackagePhotoOptional()
+        }
+        txtPhotoAction.setOnClickListener {
+            handlePhotoActionClicked {
                 addPackagePhotoOptional()
             }
-            txtPhotoAction.setOnClickListener {
-                addPackagePhotoOptional()
-            }
-            imgPhoto.setOnClickListener {
-                selectedImage?.let { photo ->
-                    previewPhoto?.invoke(photo)
-                }
+        }
+        imgPhoto.setOnClickListener {
+            selectedImage?.let { file ->
+                previewPhotoCallback?.invoke(file)
             }
         }
     }
@@ -70,8 +100,11 @@ class LPAttachFile : FrameLayout {
         activity?.let {
             fileUri = ImageUtils.getOutputMediaFileUri(
                 activity = it,
-                resources.getString(R.string.claim_form_page_photo_ktp_form)
+                R.string.claim_form_page_photo_ktp_form.toString()
             )
+            fileUri?.let { uri ->
+                gotoCameraCallback?.invoke(uri)
+            }
         }
     }
 
@@ -79,8 +112,11 @@ class LPAttachFile : FrameLayout {
         activity?.let {
             fileUri = ImageUtils.getOutputMediaFileUri(
                 activity = it,
-                resources.getString(R.string.claim_form_select_image_title)
+                R.string.claim_form_page_photo_ktp_form.toString()
             )
+            fileUri?.let { uri ->
+                gotoGalleryCallback?.invoke(uri)
+            }
         }
     }
 
@@ -106,49 +142,53 @@ class LPAttachFile : FrameLayout {
         builder.show()
     }
 
-//    private fun handlePhotoActionClicked(containerView: View, addPhotoCallback: () -> Unit) {
-//        if (containerView.txtPhotoAction.text == resources.getString(R.string.general_change)) {
-//            addPhotoCallback.invoke()
-//        } else resetViewPhotoDetail(containerView)
-//    }
-//
-//    private fun resetViewPhotoDetail(containerView: View) {
-//        val mapKey = getMapKeyByContainerView(containerView)
-//        containerView.llEmptyPhoto.isVisible = true
-//        containerView.clPhotoDetail.isVisible = false
-//        changeStateViewPhotoField(false, containerView)
-//        containerView.txtPhotoName.text = String()
-//        containerView.txtPhotoSize.text = String()
-//        containerView.txtPhotoAction.text = resources.getString(R.string.general_change)
-//    }
-//
-//    private fun getMapKeyByContainerView(containerView: View): String {
-//        return when (containerView) {
-//            includeKtpPhoto -> KTP_PHOTO_KEY
-//            includePackagePhotoOptional -> PACKAGE_PHOTO_OPTIONAL_KEY
-//            else -> PACKAGE_PHOTO_REQUIRED_KEY
-//        }
-//    }
+    private fun handlePhotoActionClicked(addPhotoCallback: () -> Unit) {
+        if (txtPhotoAction.text == resources.getString(R.string.general_change)) {
+            addPhotoCallback.invoke()
+        } else resetViewPhotoDetail()
+    }
+
+    private fun resetViewPhotoDetail() {
+        onResetPhotoCallback?.invoke(selectedImage, fileUri)
+        llEmptyPhoto.isVisible = true
+        clPhotoDetail.isVisible = false
+        changeStateViewPhotoField(false)
+        txtPhotoName.text = String()
+        txtPhotoSize.text = String()
+        txtPhotoAction.text = resources.getString(R.string.general_change)
+        selectedImage = null
+        fileUri = null
+        changeStateViewPackagePhotoOptional()
+    }
+
+    private fun changeStateViewPackagePhotoOptional() {
+        llEmptyPhoto.isEnabled = isPhotoEnabled
+        clParent.alpha = if (isPhotoEnabled) 1.0F else 0.5F
+    }
 
     fun changeStateViewPhotoField(
-        isError: Boolean,
-        containerView: View
+        isError: Boolean
     ) {
         val textColor = if (isError) R.color.interpack6 else R.color.shades3
         val maxPhotoLabel = when {
-            isError && !containerView.llEmptyPhoto.isVisible -> resources.getString(R.string.form_claim_damaged_or_lost_photo_invalid_error)
+            isError && !llEmptyPhoto.isVisible -> resources.getString(R.string.form_claim_damaged_or_lost_photo_invalid_error)
             else -> resources.getString(R.string.form_claim_damaged_or_lost_max_spec_photo_label)
         }
         val requiredLabel = when {
-            isError && containerView.llEmptyPhoto.isVisible -> resources.getString(R.string.form_claim_damaged_or_lost_photo_empty_error)
+            isError && llEmptyPhoto.isVisible -> resources.getString(R.string.form_claim_damaged_or_lost_photo_empty_error)
             else -> resources.getString(R.string.form_claim_damaged_or_lost_required_label)
         }
-        containerView.isSelected = isError
+        llKtpPhoto.isSelected = isError
         when (containerView) {
             llKtpPhoto -> {
                 txtKtpPhotoLabel.changeTextColor(textColor)
                 txtKtpPhotoMaxLabel.changeTextAndColor(maxPhotoLabel, textColor)
                 txtRequiredKtpPhotoLabel.changeTextAndColor(requiredLabel, textColor)
+            }
+
+            llPackagePhotoOptional -> {
+                txtPackagePhotoOptionalMaxLabel.changeTextAndColor(maxPhotoLabel, textColor)
+                txtOptionalPackagePhotoLabel.changeTextColor(textColor)
             }
         }
     }
@@ -161,4 +201,5 @@ class LPAttachFile : FrameLayout {
         text = textString
         changeTextColor(color)
     }
+
 }
--- sample/src/main/java/com/lionparcel/commonandroidsample/form/AttachFileActivity.kt
+++ sample/src/main/java/com/lionparcel/commonandroidsample/form/AttachFileActivity.kt
@@ -1,7 +1,9 @@
 package com.lionparcel.commonandroidsample.form
 
-import android.os.Bundle
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
 import com.lionparcel.commonandroid.form.LPAttachFile
 import com.lionparcel.commonandroidsample.R
 
@@ -12,7 +14,15 @@ class AttachFileActivity : AppCompatActivity() {
         findViewById<LPAttachFile>(R.id.lpAttachFile).apply {
             activity = this@AttachFileActivity
             gotoGallery = {
-
+                startActivity(
+                    Intent.createChooser(
+                        Intent(
+                            Intent.ACTION_PICK,
+                            MediaStore.Images.Media.INTERNAL_CONTENT_URI
+                        ).setType("image/*"),
+                        getString(R.string.claim_form_select_image_title)
+                    )
+                )
             }
         }
     }
