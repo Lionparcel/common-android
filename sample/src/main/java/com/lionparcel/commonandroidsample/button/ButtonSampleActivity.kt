c7467705e584e93a425e1beed0ef06c758d06f9c
ushisui45 <58464856+ushisui45@users.noreply.github.com>
2022-02-04 02:10:21 +0700

    add button sample activity

---
+++ sample/src/main/java/com/lionparcel/commonandroidsample/button/ButtonSampleActivity.kt
@@ -0,0 +1,12 @@
+package com.lionparcel.commonandroidsample.button
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lionparcel.commonandroidsample.R
+
+class ButtonSampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_button_sample)
+    }
+}
