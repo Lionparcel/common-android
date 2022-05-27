a24d7b4af77136604a7b3d45c8c5f34305df24cb
naufal22hadi <naufal22hadi@gmail.com>
2022-05-27 14:02:55 +0700

    first commit

---
+++ core/src/main/java/com/lionparcel/commonandroid/form/LPTextArea.kt
@@ -0,0 +1,12 @@
+package com.lionparcel.commonandroid.form
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class LPTextArea @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+) : ConstraintLayout(context, attrs, defStyleAttr)  {
+}
