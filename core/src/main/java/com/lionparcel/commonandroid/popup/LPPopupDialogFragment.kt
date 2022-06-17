218b6125fb83032e88b641dae6ee52c5fbaacb83
naufal22hadi <naufal22hadi@gmail.com>
2022-06-17 13:52:07 +0700

    LPDS-16 add layout for popup dialog

---
+++ core/src/main/java/com/lionparcel/commonandroid/popup/LPPopupDialogFragment.kt
@@ -0,0 +1,9 @@
+package com.lionparcel.commonandroid.popup
+
+import com.lionparcel.commonandroid.modal.TypeModal
+import com.lionparcel.commonandroid.popup.base.BaseDialogFragment
+
+class LPPopupDialogFragment(private val typeModal: TypeModal) : BaseDialogFragment{
+
+    override fun getContentResource(): Int =
+}
