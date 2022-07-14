a69c516a016c85a677d2cff2801749d1c6cd9010
naufal22hadi <naufal22hadi@gmail.com>
2022-07-15 03:10:21 +0700

    Add LPStepperIcon and implement in sample activity

--- core/src/main/java/com/lionparcel/commonandroid/stepper/utils/StepperStatus.kt
+++ core/src/main/java/com/lionparcel/commonandroid/stepper/utils/StepperStatus.kt
@@ -1,5 +1,8 @@
 package com.lionparcel.commonandroid.stepper.utils
 
+import android.widget.ImageView
+import com.lionparcel.commonandroid.R
+
 enum class StepperDotState(val value : String) {
     PRIMARY("PRIMARY"),
     DEFAULT("DEFAULT"),
@@ -12,9 +15,17 @@ enum class StepperDotPosition(val value: String) {
     LAST("LAST")
 }
 
-enum class StepperNumberState(val value: String){
+enum class StepperNumberState(val value: String) {
     INACTIVE("INACTIVE"),
     ACTIVE("ACTIVE"),
     FINISHED("FINISHED"),
     FAILED("FAILED")
+}
+
+enum class StepperIconState(val value: String) {
+    INACTIVE("INACTIVE"),
+    ACTIVE("ACTIVE"),
+    SUCCESS("SUCCESS"),
+    FAILED("FAILED"),
+    WARNING("WARNING")
 }
