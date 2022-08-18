e74c5c5313de5d3c8ff0b8db579cec850ea24563
naufal22hadi <naufal22hadi@gmail.com>
2022-08-19 03:24:01 +0700

    LPDS-72 change background and add multiple function

--- core/src/main/java/com/lionparcel/commonandroid/alert/LPAlert.kt
+++ core/src/main/java/com/lionparcel/commonandroid/alert/LPAlert.kt
@@ -2,18 +2,17 @@ package com.lionparcel.commonandroid.alert
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.DashPathEffect
-import android.graphics.Paint
-import android.graphics.drawable.LayerDrawable
-import android.graphics.drawable.ShapeDrawable
-import android.graphics.drawable.shapes.RoundRectShape
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import androidx.core.widget.ImageViewCompat
 import com.lionparcel.commonandroid.R
 import com.lionparcel.commonandroid.alert.utils.AlertState
 import com.lionparcel.commonandroid.databinding.LpLayoutAlertBinding
+import com.lionparcel.commonandroid.form.utils.setRegularFont
+import com.lionparcel.commonandroid.form.utils.setSemiBoldFont
+import com.lionparcel.commonandroid.form.utils.setSemiBoldSpannable
 
 class LPAlert @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttrs: Int = 0
@@ -22,6 +21,7 @@ class LPAlert @JvmOverloads constructor(
     private val binding: LpLayoutAlertBinding = LpLayoutAlertBinding.inflate(LayoutInflater.from(context), this, true)
 
     private var size : Int
+    private var style: Int
 
     init {
         context.theme.obtainStyledAttributes(
@@ -32,12 +32,14 @@ class LPAlert @JvmOverloads constructor(
         ).apply {
             try {
                 size = getInt(R.styleable.LPAlert_alertSize, 0)
+                style = getInt(R.styleable.LPAlert_alertStyle, 0)
             } finally {
                 recycle()
             }
         }
         binding.root
         setAlertSize()
+        setAlertStyle()
     }
 
     private fun setAlertSize() {
@@ -46,34 +48,51 @@ class LPAlert @JvmOverloads constructor(
             0 -> {
                 binding.llAlert.setPadding((16 * scale).toInt(), (12 * scale).toInt(), (16 * scale).toInt(), (12 * scale).toInt())
                 binding.iconStartAlert.apply {
-                    val params = LayoutParams(this.layoutParams)
+                    val params = this.layoutParams as MarginLayoutParams
                     layoutParams.height = (20 * scale).toInt()
                     layoutParams.width = (20 * scale).toInt()
                     params.setMargins(0, 0,(12 * scale).toInt(), 0)
+                    layoutParams = params
                 }
                 binding.titleAlert.textSize = 12F
                 binding.iconEndAlert.apply {
-                    val params = LayoutParams(this.layoutParams)
+                    val params = this.layoutParams as MarginLayoutParams
                     layoutParams.height = (20 * scale).toInt()
                     layoutParams.width = (20 * scale).toInt()
                     params.setMargins((12 * scale).toInt(), 0,0, 0)
+                    layoutParams = params
                 }
             }
             1 -> {
                 binding.llAlert.setPadding((8 * scale).toInt(), (8 * scale).toInt(), (8 * scale).toInt(), (8 * scale).toInt())
                 binding.iconStartAlert.apply {
-                    val params = LayoutParams(this.layoutParams)
+                    val params = this.layoutParams as MarginLayoutParams
                     layoutParams.height = (16 * scale).toInt()
                     layoutParams.width = (16 * scale).toInt()
                     params.setMargins(0, 0,(8 * scale).toInt(), 0)
+                    layoutParams = params
                 }
                 binding.titleAlert.textSize = 10F
                 binding.iconEndAlert.apply {
-                    val params = LayoutParams(this.layoutParams)
+                    val params = this.layoutParams as MarginLayoutParams
                     layoutParams.height = (16 * scale).toInt()
                     layoutParams.width = (16 * scale).toInt()
                     params.setMargins((8 * scale).toInt(), 0,0, 0)
+                    layoutParams = params
                 }
+            }
+        }
+    }
+
+    private fun setAlertStyle() {
+        when (style) {
+            0 -> {
+                binding.contentAlert.visibility = GONE
+                binding.titleAlert.setRegularFont()
+            }
+            1 -> {
+                binding.contentAlert.visibility = VISIBLE
+                binding.titleAlert.setSemiBoldFont()
             }
         }
     }
@@ -81,73 +100,61 @@ class LPAlert @JvmOverloads constructor(
     @Suppress("DEPRECATION")
     @SuppressLint("UseCompatLoadingForColorStateLists")
     fun setAlertState(state: AlertState) {
-        val scale = resources.displayMetrics.density
-        val cornerRadius = (8 * scale)
-        val strokeWidth = (2 * scale)
-        val dashWidth = (8 * scale)
-        val dashGap = (8 * scale)
         when (state) {
             AlertState.NORMAL -> {
-                val shapeDrawable = ShapeDrawable().apply {
-                    shape = RoundRectShape(floatArrayOf(cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius), null, null)
-                    paint.style = Paint.Style.STROKE
-                    paint.strokeWidth = strokeWidth
-                    paint.pathEffect = DashPathEffect(floatArrayOf(dashGap, dashWidth),0F)
-                    paint.color = resources.getColor(R.color.shades2)
-                }
-                val shapeFill = ShapeDrawable().apply {
-                    shape = RoundRectShape(floatArrayOf(cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius), null, null)
-                    paint.style = Paint.Style.FILL_AND_STROKE
-                    paint.color = resources.getColor(R.color.white)
-                }
-                val composite = LayerDrawable(arrayOf(shapeFill, shapeDrawable))
-                binding.llAlert.background = composite
+                binding.llAlert.background = ContextCompat.getDrawable(context, R.drawable.bg_alert_normal_dash)
                 binding.titleAlert.setTextColor(resources.getColor(R.color.shades5))
+                binding.contentAlert.setTextColor(resources.getColor(R.color.shades5))
                 ImageViewCompat.setImageTintList(binding.iconStartAlert, resources.getColorStateList(R.color.shades5))
                 ImageViewCompat.setImageTintList(binding.iconEndAlert, resources.getColorStateList(R.color.shades5))
             }
             AlertState.WARNING -> {
-                val shapeDrawable = ShapeDrawable().apply {
-                    shape = RoundRectShape(floatArrayOf(cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius), null, null)
-                    paint.style = Paint.Style.STROKE
-                    paint.strokeWidth = strokeWidth
-                    paint.pathEffect = DashPathEffect(floatArrayOf(dashGap, dashWidth), dashWidth)
-                    paint.color = resources.getColor(R.color.yellow6)
-                }
-                val shapeFill = ShapeDrawable().apply {
-                    shape = RoundRectShape(floatArrayOf(cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius), null, null)
-                    paint.style = Paint.Style.FILL_AND_STROKE
-                    paint.color = resources.getColor(R.color.yellow1)
-                }
-                val composite = LayerDrawable(arrayOf(shapeFill, shapeDrawable))
-                binding.llAlert.background = composite
+                binding.llAlert.background = ContextCompat.getDrawable(context, R.drawable.bg_alert_warning_dash)
                 binding.titleAlert.setTextColor(resources.getColor(R.color.box7))
+                binding.contentAlert.setTextColor(resources.getColor(R.color.box7))
                 ImageViewCompat.setImageTintList(binding.iconStartAlert, resources.getColorStateList(R.color.yellow6))
                 ImageViewCompat.setImageTintList(binding.iconEndAlert, resources.getColorStateList(R.color.yellow6))
             }
             AlertState.DANGER -> {
-                val shapeDrawable = ShapeDrawable().apply {
-                    shape = RoundRectShape(floatArrayOf(cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius), null, null)
-                    paint.style = Paint.Style.STROKE
-                    paint.strokeWidth = strokeWidth
-                    paint.pathEffect = DashPathEffect(floatArrayOf(dashGap, dashWidth), dashWidth)
-                    paint.color = resources.getColor(R.color.interpack6)
-                }
-                val shapeFill = ShapeDrawable().apply {
-                    shape = RoundRectShape(floatArrayOf(cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius), null, null)
-                    paint.style = Paint.Style.FILL_AND_STROKE
-                    paint.color = resources.getColor(R.color.red1)
-                }
-                val composite = LayerDrawable(arrayOf(shapeFill, shapeDrawable))
-                binding.llAlert.background = composite
+                binding.llAlert.background = ContextCompat.getDrawable(context, R.drawable.bg_alert_danger_dash)
                 binding.titleAlert.setTextColor(resources.getColor(R.color.interpack6))
+                binding.contentAlert.setTextColor(resources.getColor(R.color.interpack6))
                 ImageViewCompat.setImageTintList(binding.iconStartAlert, resources.getColorStateList(R.color.interpack6))
                 ImageViewCompat.setImageTintList(binding.iconEndAlert, resources.getColorStateList(R.color.interpack6))
             }
         }
     }
 
-    fun setText(text: String) {
+    fun setTextTitle(text: String) {
         binding.titleAlert.text = text
+    }
+
+    fun setTextContent(text: String) {
+        binding.contentAlert.text = text
+    }
+
+    fun setStartIcon(icon: Int = R.drawable.ics_f_warning_circle, isVisible: Boolean = true) {
+        if (isVisible) {
+            binding.iconStartAlert.visibility = VISIBLE
+        } else {
+            binding.iconStartAlert.visibility = GONE
+        }
+        binding.iconStartAlert.setImageResource(icon)
+    }
+
+    fun setEndIcon(icon: Int = R.drawable.ic_o_chevron_right, isVisible: Boolean = true) {
+        if (isVisible) {
+            binding.iconEndAlert.visibility = VISIBLE
+        } else {
+            binding.iconEndAlert.visibility = GONE
+        }
+        binding.iconEndAlert.setImageResource(icon)
+    }
+
+    fun setSemiBoldSpannable(content: CharSequence, targetString: String) {
+        when (style) {
+            0 -> binding.titleAlert.setSemiBoldSpannable(content, targetString)
+            1 -> binding.contentAlert.setSemiBoldSpannable(content, targetString)
+        }
     }
 }
--- sample/src/main/java/com/lionparcel/commonandroidsample/alert/AlertComponentSampleActivity.kt
+++ sample/src/main/java/com/lionparcel/commonandroidsample/alert/AlertComponentSampleActivity.kt
@@ -12,15 +12,53 @@ class AlertComponentSampleActivity : AppCompatActivity() {
         setContentView(R.layout.activity_alert_component_sample)
         findViewById<LPAlert>(R.id.lp_alert).apply {
             setAlertState(AlertState.NORMAL)
-            setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+            setTextTitle("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+            setSemiBoldSpannable("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "consectetur")
+            setStartIcon()
+            setEndIcon()
         }
         findViewById<LPAlert>(R.id.lp_alert_2).apply {
             setAlertState(AlertState.WARNING)
-            setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+            setTextTitle("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+            setEndIcon()
         }
         findViewById<LPAlert>(R.id.lp_alert_3).apply {
             setAlertState(AlertState.DANGER)
-            setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+            setTextTitle("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+        }
+        findViewById<LPAlert>(R.id.lp_alert_small_1).apply {
+            setAlertState(AlertState.NORMAL)
+            setTextTitle("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisi duis iaculis viverra quam.")
+        }
+        findViewById<LPAlert>(R.id.lp_alert_small_2).apply {
+            setAlertState(AlertState.WARNING)
+            setTextTitle("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisi duis iaculis viverra quam.")
+            setStartIcon()
+        }
+        findViewById<LPAlert>(R.id.lp_alert_small_3).apply {
+            setAlertState(AlertState.DANGER)
+            setTextTitle("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisi duis iaculis viverra quam.")
+            setEndIcon()
+        }
+
+        findViewById<LPAlert>(R.id.lp_alert_title_1).apply {
+            setAlertState(AlertState.NORMAL)
+            setTextTitle("Vitae sed elementum lacus.")
+            setTextContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisi duis iaculis viverra quam.")
+        }
+        findViewById<LPAlert>(R.id.lp_alert_title_2).apply {
+            setAlertState(AlertState.WARNING)
+            setTextTitle("Vitae sed elementum lacus.")
+            setTextContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisi duis iaculis viverra quam.")
+            setStartIcon()
+            setEndIcon()
+        }
+        findViewById<LPAlert>(R.id.lp_alert_title_3).apply {
+            setAlertState(AlertState.DANGER)
+            setTextTitle("Vitae sed elementum lacus.")
+            setTextContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisi duis iaculis viverra quam.")
+            setSemiBoldSpannable("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisi duis iaculis viverra quam.", "adipiscing")
+            setEndIcon()
         }
     }
 }
