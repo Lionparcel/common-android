bacde1d20b140354d9e7a5e1eb2bb9de404517dd
naufal22hadi <naufal22hadi@gmail.com>
2022-09-01 11:46:05 +0700

    LPDS-80 add more preview in sample

--- sample/src/main/java/com/lionparcel/commonandroidsample/badge/BadgeComponentSampleActivity.kt
+++ sample/src/main/java/com/lionparcel/commonandroidsample/badge/BadgeComponentSampleActivity.kt
@@ -11,10 +11,15 @@ class BadgeComponentSampleActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_badge_component_sample)
         findViewById<LPBadgeNumber>(R.id.lpBadgeNumber1).setNumber("5")
-        findViewById<LPBadgeNumber>(R.id.lpBadgeNumber2).setNumber("5")
+        findViewById<LPBadgeNumber>(R.id.lpBadgeNumber2).setNumber("10")
         findViewById<LPBadgeNumber>(R.id.lpBadgeNumber3).setNumber("99+")
         findViewById<LPBadgeNumber>(R.id.lpBadgeNumber4).setNumber("99+")
+        findViewById<LPBadgeNumber>(R.id.lpBadgeNumber5).setNumber("5")
+        findViewById<LPBadgeNumber>(R.id.lpBadgeNumber6).setNumber("10")
+        findViewById<LPBadgeNumber>(R.id.lpBadgeNumber7).setNumber("99+")
+        findViewById<LPBadgeNumber>(R.id.lpBadgeNumber8).setNumber("99+")
 
         findViewById<LPBadgeText>(R.id.lpBadgeText).setTitle("Name")
+        findViewById<LPBadgeText>(R.id.lpBadgeText1).setTitle("New")
     }
 }
