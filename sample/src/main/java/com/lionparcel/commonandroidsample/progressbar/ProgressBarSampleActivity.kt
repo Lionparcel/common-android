ef855ec3a3f89a8f4f2ff933988e5d979450f25b
naufal22hadi <naufal22hadi@gmail.com>
2022-09-02 13:52:42 +0700

    LPDS-152 improve progressbar logic and update version dls

--- sample/src/main/java/com/lionparcel/commonandroidsample/progressbar/ProgressBarSampleActivity.kt
+++ sample/src/main/java/com/lionparcel/commonandroidsample/progressbar/ProgressBarSampleActivity.kt
@@ -12,16 +12,19 @@ class ProgressBarSampleActivity : AppCompatActivity() {
 
         findViewById<LPProgressBar>(R.id.pbSample1).apply {
             setProgress(100)
+            setDynamicIndicatorProgress(100F)
             setDynamicIndicatorTitle("10x")
             setFixIndicatorTitle("10x")
         }
         findViewById<LPProgressBar>(R.id.pbSample2).apply {
             setProgress(50)
+            setDynamicIndicatorProgress(50F)
             setDynamicIndicatorTitle("5x")
             setFixIndicatorTitle("10x")
         }
         findViewById<LPProgressBar>(R.id.pbSample3).apply {
             setProgress(0)
+            setDynamicIndicatorProgress(0F)
             setDynamicIndicatorTitle("0x")
             setFixIndicatorTitle("10x")
         }
