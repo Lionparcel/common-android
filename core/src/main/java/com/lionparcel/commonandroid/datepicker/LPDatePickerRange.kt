5a37fe18e016f8171f77fd9b471f8a39c590b40c
naufal22hadi <naufal22hadi@gmail.com>
2022-10-03 03:19:34 +0700

    add layout and drawable res for range datepicker

---
+++ core/src/main/java/com/lionparcel/commonandroid/datepicker/LPDatePickerRange.kt
@@ -0,0 +1,9 @@
+package com.lionparcel.commonandroid.datepicker
+
+import com.lionparcel.commonandroid.datepicker.utils.BaseDatePicker
+
+class LPDatePickerRange: BaseDatePicker() {
+
+    override fun getContentResource() = -1
+
+}
