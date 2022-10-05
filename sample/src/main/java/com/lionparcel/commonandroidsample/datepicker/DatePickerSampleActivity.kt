8c84d37999db063e97625e01540b860ecb4449e7
naufal22hadi <naufal22hadi@gmail.com>
2022-10-06 02:40:37 +0700

    add and implement date picker single

--- sample/src/main/java/com/lionparcel/commonandroidsample/datepicker/DatePickerSampleActivity.kt
+++ sample/src/main/java/com/lionparcel/commonandroidsample/datepicker/DatePickerSampleActivity.kt
@@ -1,13 +1,13 @@
 package com.lionparcel.commonandroidsample.datepicker
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
 import com.lionparcel.commonandroid.datepicker.LPDatePickerRange
+import com.lionparcel.commonandroid.datepicker.LPDatePickerSingle
 import com.lionparcel.commonandroid.datepicker.utils.DateUtils
 import com.lionparcel.commonandroid.datepicker.utils.toDate
 import com.lionparcel.commonandroid.datepicker.utils.toLocaleDate
@@ -19,20 +19,27 @@ import java.util.*
 class DatePickerSampleActivity : AppCompatActivity() {
 
     companion object {
-        private const val DATE_PICKER_DIALOG = "DATE_PICKER_DIALOG"
+        private const val DATE_PICKER_RANGE_DIALOG = "DATE_PICKER_RANGE_DIALOG"
+        private const val DATE_PICKER_SINGLE_DIALOG = "DATE_PICKER_SINGLE_DIALOG"
     }
 
     private var startDate: Date? = null
     private var endDate: Date? = null
     private val dialogFragmentMap = mutableMapOf<String, WeakReference<DialogFragment>>()
 
+    private var selectedDate: Date? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_date_picker_sample)
         findViewById<LinearLayout>(R.id.llDatePicker).setOnClickListener {
-            showDatePickerDialog()
+            showDatePickerRangeDialog()
+        }
+        findViewById<LinearLayout>(R.id.llDatePicker2).setOnClickListener {
+            showDatePickerSingleDialog()
         }
         setSelectedDateView(findViewById(R.id.tvDatePick))
+        setSelectedSingleDateView(findViewById(R.id.tvDatePick2))
     }
 
     init {
@@ -43,6 +50,7 @@ class DatePickerSampleActivity : AppCompatActivity() {
             calendar.time
         }
         endDate = Date()
+        selectedDate = Date()
     }
 
     private fun setSelectedDateView(text: TextView) {
@@ -55,26 +63,50 @@ class DatePickerSampleActivity : AppCompatActivity() {
         }
     }
 
-    private fun showDatePickerDialog() {
+    private fun setSelectedSingleDateView(text: TextView) {
+        text.text = selectedDate?.let {
+            DateUtils.dateToString(it, DateUtils.DATE_FORMAT)
+        }
+    }
+
+    private fun showDatePickerRangeDialog() {
         val start = startDate ?: return
         showCustomDialog(
             supportFragmentManager,
-            DATE_PICKER_DIALOG,
+            DATE_PICKER_RANGE_DIALOG,
             LPDatePickerRange.newInstance(
                 start.toLocaleDate(),
                 endDate?.toLocaleDate(),
-                this::onChooseButtonDatePicker
+                this::onChooseButtonDatePickerRange
             )
         )
-        Log.e("e","Error" )
     }
 
-    private fun onChooseButtonDatePicker(startDate: LocalDate, endDate: LocalDate?) {
-        dialogFragmentMap[DATE_PICKER_DIALOG]?.get()?.dismissAllowingStateLoss()
-        dialogFragmentMap.remove(DATE_PICKER_DIALOG)
+    private fun showDatePickerSingleDialog() {
+        val date = selectedDate ?: return
+        showCustomDialog(
+            supportFragmentManager,
+            DATE_PICKER_SINGLE_DIALOG,
+            LPDatePickerSingle.newInstance(
+                date.toLocaleDate(),
+                this::onChooseButtonDatePickerSingle
+            )
+        )
+    }
+
+    private fun onChooseButtonDatePickerRange(startDate: LocalDate, endDate: LocalDate?) {
+        dialogFragmentMap[DATE_PICKER_RANGE_DIALOG]?.get()?.dismissAllowingStateLoss()
+        dialogFragmentMap.remove(DATE_PICKER_RANGE_DIALOG)
         this.startDate = startDate.toDate()
         this.endDate = endDate?.toDate()
         setSelectedDateView(findViewById(R.id.tvDatePick))
+    }
+
+    private fun onChooseButtonDatePickerSingle(selectedDate: LocalDate) {
+        dialogFragmentMap[DATE_PICKER_SINGLE_DIALOG]?.get()?.dismissAllowingStateLoss()
+        dialogFragmentMap.remove(DATE_PICKER_SINGLE_DIALOG)
+        this.selectedDate = selectedDate.toDate()
+        setSelectedSingleDateView(findViewById(R.id.tvDatePick2))
     }
 
     private fun showCustomDialog(
