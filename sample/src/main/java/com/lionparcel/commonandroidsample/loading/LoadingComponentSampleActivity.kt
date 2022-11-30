3c0eb38545e6a686bf238b270c438b0dea81cc61
naufal22hadi <naufal22hadi@gmail.com>
2022-12-01 00:05:50 +0700

    Add loading component for xml implementation

--- sample/src/main/java/com/lionparcel/commonandroidsample/loading/LoadingComponentSampleActivity.kt
+++ sample/src/main/java/com/lionparcel/commonandroidsample/loading/LoadingComponentSampleActivity.kt
@@ -3,8 +3,10 @@ package com.lionparcel.commonandroidsample.loading
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import com.lionparcel.commonandroid.loading.LPFullScreenSpinner
+import com.lionparcel.commonandroid.loading.LPSpinnerLoadingPage
 import com.lionparcel.commonandroidsample.R
 
 class LoadingComponentSampleActivity : AppCompatActivity() {
@@ -22,6 +24,11 @@ class LoadingComponentSampleActivity : AppCompatActivity() {
         }
         findViewById<Button>(R.id.btnLoading4).setOnClickListener {
             LPFullScreenSpinner.newInstance(this, "Loading").show()
+        }
+        findViewById<Button>(R.id.btnLoading5).setOnClickListener {
+            findViewById<LPSpinnerLoadingPage>(R.id.loadingSpinner).apply {
+                visibility = View.VISIBLE
+            }
         }
     }
 
